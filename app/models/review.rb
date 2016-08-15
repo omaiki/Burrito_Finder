0159a3e7268899f8afef5cae9e56f8233f9971b5
Oshoke Maiki <oshokemaiki@gmail.com>
2016-08-15 11:27:16 -0700

    Create add review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :burrito
   belongs_to :user
-  has_one :restaurant, :through => :burrito
   belongs_to :playground
+  has_one :restaurant, :through => :burrito
 end
