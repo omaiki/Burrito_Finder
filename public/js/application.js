136a662bab6317cf095fec62b0773b1a8fe6978c
Oshoke Maiki <oshokemaiki@gmail.com>
2016-08-19 00:35:49 -0700

    Add bootstrap css to project, nav bar working, reviews look better

--- public/js/application.js
+++ public/js/application.js
@@ -8,20 +8,6 @@
 
 
 
-// function codeAddress() {
-//   var address = document.getElementById('address').value;
-//   geocoder.geocode( {'address': address}, function(results, status) {
-//     if (status == 'OK') {
-//       map.setCenter(results[0].geometry.location);
-//       var marker = new google.maps.Marker({
-//           map: map,
-//           position: results[0].geometry.location,
-//           icon: 'https://s3.amazonaws.com/herokubobafetch/Boba-Fett-icon.png'
-//       });
-//     } else {
-//       alert('Geocode was not successful for the following reason: ' + status);
-//     }
-//   });
-// }
+
 
 
